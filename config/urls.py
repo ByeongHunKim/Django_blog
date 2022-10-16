127021f23f76146d167d18b9ddee0f1bfef35650
ByeongHunKim <rkwhr912@naver.com>
2022-10-17 00:00:39 +0900

    add : webpage application, djangorestframework

--- config/urls.py
+++ config/urls.py
@@ -13,9 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from webpage.views import Main
+
+from config import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # url 뒤에 아무도 없다 -> url만 접속했을 떄 Main class 실행
+    path('', Main.as_view()),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_route=settings.STATIC_URL)
